3f1aa2bcd88119d3b0d7ddfc6d034420e1d450a0
kumarisetu258@gmail.com <singhpratik015@gmail.com>
2024-04-29 18:23:50 +0530

    from master

---
+++ add1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define PI 3.14
+#define mod 1e9+7
+#define int int64_t
+#define endl '\n'
+#define pb push_back
+#define pop() pop_back()
+#define sort_v_dec() sort(v.begin(),v.end(),greater<int>())
+#define sort_v_inc() sort(v.begin(),v.end())
+#define lb lower_bound
+#define read(num)  vector<int> v(num); for(auto &p:v){cin>>p;}
+#define print(num)   for(auto &p:v){cout<<p;}
+
+
+bool isPowerOfTwo (int x)
+{
+return x && (!(x&(x-1)));
+}
+void solve(){
+
+}
+signed main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+    return 0;
+}
